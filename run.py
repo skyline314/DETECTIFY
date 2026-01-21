b5d3897877e4d2f92effc4005528262fa0805f6a
Kenny Wijaya <kennywijaya123crb@gmail.com>
2026-01-21 17:00:04 +0700

    Final: Restore Console v7 Hybrid with Snap Midtrans and JSON Auth Fix and create table

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from app import create_app
+from app import create_app, db
 
 # Muat konfigurasi dari env variable 'FLASK_ENV' (production/development)
 config_name = os.getenv('FLASK_ENV', 'production')
@@ -11,4 +11,7 @@ app.config['SERVER_NAME'] = None
 app.config['PREFERRED_URL_SCHEME'] = 'https'
 
 if __name__ == '__main__':
+    with app.app_context():
+        # Perintah ini hanya akan membuat tabel jika tabel belum ada
+        db.create_all()
     app.run()
